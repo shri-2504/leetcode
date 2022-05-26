b2203fe950f1bc8a70dd7ba583a9f6014ac38e73
Shubham <90036500+shri-2504@users.noreply.github.com>
2022-05-26 14:03:04 +0530

    Added solution - LeetHub

--- Implement Atoi - GFG/implement-atoi.cpp
+++ Implement Atoi - GFG/implement-atoi.cpp
@@ -17,15 +17,16 @@ class Solution{
         int ans=0,count=0;
         int neg=1;
         for(int i=0;i<n;i++){
-         if(i==0&& str[i]=='-'){neg=-1; continue;}
+         if(i==0&& str[i]=='-'){neg=-1;count++; continue;}
             result=str[i]-'0';
             if(result>=0 && result<=9){
                 ans=ans*10+result;
                 count++;
             }
-            else return -1;
+            // else return -1;
         }
-        return neg*ans;
+        if(count==n) return neg*ans;
+       return -1;
     }
 };
 
